5396a44d1570fb804ce10f2ce00e02e6358e11a1
Vichitra  Pool <notejde@gmail.com>
2016-09-30 00:29:40 -0700

    Update Band_Tests.cs

---
+++ Tests/Band_Tests.cs
@@ -0,0 +1,12 @@
+using System;
+using Xunit;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BandTracker
+{
+  public class BandTests : IDisposable
+  {
+    public BandTests()
+    {
