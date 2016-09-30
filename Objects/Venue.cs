1c1f44cd5cf58eff2ca845701cd9ac9e825d2cb6
vp <notejde@gmail.com>
2016-09-30 02:07:54 -0700

    add getter & setter for venue

--- Objects/Venue.cs
+++ Objects/Venue.cs
@@ -7,3 +7,26 @@ namespace BandTracker
 {
   public class Venue
   {
+    //properties
+    private int _id;
+    private string _location;
+    //constructor (id set = 0 the same in Sql)
+    public Venue(string location, int id = 0);
+    {
+      _id = id;
+      _location = location;
+    }
+
+    public int GetId()
+    {
+      return _id;
+    }
+    public string GetLocation()
+    {
+      return _location;
+    }
+    public void SetLocation(string newLocation)
+    {
+      _location = newLocation;
+    }
+    
