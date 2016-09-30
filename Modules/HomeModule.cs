cc8d6cf7f50fa8a3ebe1fa36e3e5fc128d868dd8
Vichitra  Pool <notejde@gmail.com>
2016-09-30 00:27:15 -0700

    Update HomeModule.cs

---
+++ Modules/HomeModule.cs
@@ -0,0 +1,15 @@
+using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using Nancy.ViewEngines.Razor;
+
+namespace BandTracker
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      Get["/"] = _ => {
+        return View["index.cshtml"];
+      };
