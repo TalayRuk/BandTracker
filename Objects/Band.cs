35954e918ac57f79f8b6cd3d63242a6d2c1143ab
vp <notejde@gmail.com>
2016-09-30 02:15:50 -0700

    add getter & setter for band

--- Objects/Band.cs
+++ Objects/Band.cs
@@ -7,3 +7,35 @@ namespace BandTracker
 {
   public class Band
   {
+    //properties
+    private int _id;
+    private string _name;
+    private DateTime _showTime;
+    //constructor (id set = 0 the same in Sql)
+    public Venue(string name, DateTime showTime, int id = 0);
+    {
+      _id = id;
+      _name = name;
+      _showTime = showTime;
+    }
+
+    public int GetId()
+    {
+      return _id;
+    }
+    public string GetName()
+    {
+      return _name;
+    }
+    public DateTime GetShowTime()
+    {
+      return _showTime;
+    }
+    public void SetName(string newName)
+    {
+      _name = newName;
+    }
+    public void SetShowTime(DateTime newShowTime)
+    {
+      _showTime = newShowTime;
+    }
